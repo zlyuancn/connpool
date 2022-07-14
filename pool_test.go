58d9c70d505596eacfcc08f2323ff6c0fd5d7881
zlyuanzhang <zlyuanzhang@tencent.com>
2022-07-14 11:53:04 +0800

    添加单测

--- pool_test.go
+++ pool_test.go
@@ -48,19 +48,65 @@ func TestGet(t *testing.T) {
 	p.Close()
 }
 
+func TestGetPoolClose(t *testing.T) {
+	conf := makeTestConfig()
+	p, err := NewConnectPool(conf)
+	require.Nil(t, err)
+	p.Close()
+
+	_, err = p.Get(context.Background())
+	require.Equal(t, ErrPoolClosed, err)
+}
+
 func TestGetTimeout(t *testing.T) {
 	conf := makeTestConfig()
-	conf.MaxActive = 2
+	conf.MaxActive = 1
 	conf.WaitTimeout = time.Millisecond * 300
 	p, err := NewConnectPool(conf)
 	require.Nil(t, err)
 
 	_, err = p.Get(context.Background())
 	require.Nil(t, err)
+
 	_, err = p.Get(context.Background())
+	require.Equal(t, ErrWaitGetConnTimeout, err)
+	p.Close()
+}
+
+func TestGetTimeoutCtx(t *testing.T) {
+	conf := makeTestConfig()
+	conf.MaxActive = 1
+	p, err := NewConnectPool(conf)
 	require.Nil(t, err)
 
 	_, err = p.Get(context.Background())
-	require.NotNil(t, ErrWaitGetConnTimeout)
+	require.Nil(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
+	defer cancel()
+	_, err = p.Get(ctx)
+	require.Equal(t, ErrWaitGetConnTimeout, err)
+	p.Close()
+}
+
+func TestGetLimitWaitCount(t *testing.T) {
+	conf := makeTestConfig()
+	conf.MaxActive = 1
+	conf.MaxWaitConnCount = 1
+	p, err := NewConnectPool(conf)
+	require.Nil(t, err)
+
+	_, err = p.Get(context.Background())
+	require.Nil(t, err)
+
+	go func() {
+		// 占位
+		_, err = p.Get(context.Background())
+		require.Equal(t, ErrPoolClosed, err)
+	}()
+
+	time.Sleep(time.Millisecond * 200) // 这里让上面的goroutinue先执行
+	_, err = p.Get(context.Background())
+	require.Equal(t, ErrMaxWaitConnLimit, err)
 	p.Close()
 }
